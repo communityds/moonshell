0b3662ca9267697298fa4d9ea7423f9f41765a8e
Phil Ingram <phil@communityds.com.au>
2017-07-05 11:54:13 +0930

    private.sh: enable the private dirs to be symlinks

--- lib/private.sh
+++ lib/private.sh
@@ -1,7 +1,8 @@
 #
 # PRIVATE FUNCTION CHAINLOADING
 #
-if [[ -d ${ENV_LIB}/private ]]; then
+# Note: 'private' must be a directory
+if [[ -d ${ENV_LIB}/private ]] || [[ -L "${ENV_LIB}/private" ]] ; then
     for private_file in $(find "${ENV_LIB}/private/" ${ENV_FIND_OPTS} -name '*.sh'); do
         source ${private_file}
     done
