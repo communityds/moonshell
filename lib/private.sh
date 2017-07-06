4da860fe86cf3e69b6b8b15d3f898c9c73e6d58a
Phil Ingram <phil@communityds.com.au>
2017-07-06 15:54:34 +0930

    private.sh: improve comments for user friendliness

--- lib/private.sh
+++ lib/private.sh
@@ -1,7 +1,10 @@
 #
 # PRIVATE FUNCTION CHAINLOADING
 #
-# Note: 'private' must be a directory
+# 'private' is a directory that is excluded from git. If you wish to use
+# private libraries, just create the directory, or symlink to a location
+# where you are tracking your senstive files.
+#
 if [[ -d ${ENV_LIB}/private ]] || [[ -L "${ENV_LIB}/private" ]] ; then
     for private_file in $(find "${ENV_LIB}/private/" ${ENV_FIND_OPTS} -name '*.sh'); do
         source ${private_file}
--- profile.d/private.sh
+++ profile.d/private.sh
@@ -1,7 +1,10 @@
 #
 # PRIVATE VARIABLE CHAINLOADING
 #
-# Note: 'private' must be a directory
+# 'private' is a directory that is excluded from git. If you wish to use
+# private libraries, just create the directory, or symlink to a location
+# where you are tracking your senstive files.
+#
 if [[ -d "${ENV_PROFILE}/private" ]] || [[ -L "${ENV_PROFILE}/private" ]]; then
     for private_file in $(find "${ENV_PROFILE}/private/" ${ENV_FIND_OPTS} -name '*.sh'); do
         source ${private_file}
