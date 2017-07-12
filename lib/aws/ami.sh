4b5e1a4d53b8c356cf5689ed6ce5cac96ca8e7e5
Phil Ingram <phil@communityds.com.au>
2017-07-12 16:36:48 +0930

    enhance ami tools

--- lib/aws/ami.sh
+++ lib/aws/ami.sh
@@ -1,20 +1,6 @@
 #
 # AMI FUNCTIONS
 #
-ami_export () {
-    local ami_id=$1
-
-    local account
-    local accounts=($(aws_accounts))
-
-    echoerr "INFO: Modifying launch permissions for ami '${ami_id}'"
-    for account in ${accounts[@]-}; do
-        aws ec2 modify-image-attribute \
-            --image-id ${ami_id} \
-            --launch-permission "{ \"Add\": [{ \"UserId\": \"${account}\" }] }"
-    done
-}
-
 ami_describe () {
     local ami_id=$1
     echoerr "INFO: Echoing launch permissions for ami '${ami_id}'"
@@ -23,6 +9,16 @@ ami_describe () {
         --image-id ${ami_id} \
         --attribute launchPermission \
         --output table
+}
+
+ami_describe () {
+    local ami_ids=$@
+
+    echoerr "INFO: Describing AMIs"
+    aws ec2 describe-images \
+        --image-ids ${ami_ids} \
+        --output table
+    return $?
 }
 
 ami_deregister () {
@@ -44,6 +40,35 @@ ami_deregister () {
         || echoerr "ERROR: Failed to deregister ${ami_id}. ${snapshot_id} is preserved"
 }
 
+ami_export () {
+    local ami_id=$1
+
+    local account
+    local accounts=($(aws_accounts))
+
+    echoerr "INFO: Modifying launch permissions for ami '${ami_id}'"
+    for account in ${accounts[@]-}; do
+        aws ec2 modify-image-attribute \
+            --image-id ${ami_id} \
+            --launch-permission "{ \"Add\": [{ \"UserId\": \"${account}\" }] }"
+    done
+}
+
+ami_find_roles () {
+    local account_id=$(sts_account_id)
+
+    echoerr "INFO: Finding all AMIs roles"
+    aws ec2 describe-images \
+        --filter \
+            Name=owner-id,Values=${account_id} \
+            Name=state,Values=available \
+        --query "Images[].Tags[?Key=='role'].Value" \
+        --output text \
+        | sort \
+        | uniq
+    return $?
+}
+
 ami_list_all () {
     # Return an array of AMI ids
     local account_id=$(sts_account_id)
@@ -53,6 +78,27 @@ ami_list_all () {
         --filter \
             Name=owner-id,Values=${account_id} \
             Name=state,Values=available \
+        --query "Images[].ImageId" \
+        --output text
+    return $?
+}
+
+ami_list_role () {
+    # Return an array of AMI ids
+    local ami_role="$1"
+    local account_id=$(sts_account_id)
+
+    if ! contains ${ami_role} $(ami_find_roles); then
+        echoerr "ERROR: No AMIs of role ${ami_role} could be found in ${account_id}"
+        return 1
+    fi
+
+    echoerr "INFO: Finding all ${ami_role} AMIs owned by ${account_id}"
+    aws ec2 describe-images \
+        --filter \
+            Name=owner-id,Values=${account_id} \
+            Name=state,Values=available \
+            Name=tag:role,Values="${ami_role}" \
         --query "Images[].ImageId" \
         --output text
     return $?
