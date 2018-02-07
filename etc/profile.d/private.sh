0f9322fa894a24f7931128bfc004e159886eb026
Phil Ingram <phil@communityds.com.au>
2018-02-07 14:39:09 +1030

    Ensure private dir is created on first sourcing of private.sh, if not exist

--- etc/profile.d/private.sh
+++ etc/profile.d/private.sh
@@ -2,10 +2,12 @@
 # PRIVATE VARIABLE CHAINLOADING
 #
 # 'private' is a directory that is excluded from git. If you wish to use
-# private libraries, just create the directory, or symlink to a location
-# where you are tracking your senstive files.
+# private libraries you can create shell files in this location, or use
+# the more preferred overlay_dir.
 #
-if [[ -d "${MOON_PROFILE}/private" ]] || [[ -L "${MOON_PROFILE}/private" ]]; then
+if [[ -d "${MOON_PROFILE}/private" ]]; then
     _moonshell_source ${MOON_PROFILE}/private
+else
+    mkdir -p "${MOON_PROFILE}/private"
 fi
 
--- lib/private.sh
+++ lib/private.sh
@@ -2,10 +2,13 @@
 # PRIVATE FUNCTION CHAINLOADING
 #
 # 'private' is a directory that is excluded from git. If you wish to use
-# private libraries, just create the directory, or symlink to a location
-# where you are tracking your senstive files.
+# private libraries you can create shell files in this location, or use
+# the more preferred overlay_dir.
 #
-if [[ -d ${MOON_LIB}/private ]] || [[ -L "${MOON_LIB}/private" ]] ; then
+
+if [[ -d ${MOON_LIB}/private ]]; then
     _moonshell_source ${MOON_LIB}/private
+else
+    mkdir -p "${MOON_LIB}/private"
 fi
 
