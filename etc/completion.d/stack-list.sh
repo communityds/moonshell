fe05dbb9817d5a8e7c1084d81ef63aef67510159
Phil Ingram <phil@communityds.com.au>
2018-05-07 09:56:33 +0930

    Enhance stack-list to only show updated/complete/rolled back stacks that are not nested

---
+++ etc/completion.d/stack-list.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# This is the autocomplete file for moonshell
+#
+
+_stack_list () {
+    local cur prev option
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    option="all parents"
+
+    if [ $COMP_CWORD -eq 1 ]; then
+        COMPREPLY=( $(compgen -W "${option}" -- ${cur}) )
+    fi
+
+    return 0
+}
+
+complete -F _stack_list stack-list
