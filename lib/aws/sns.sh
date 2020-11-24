0244a612e143ed448e135d14a4e9b438fbd75abe
Phil Ingram <phil@communityds.com.au>
2020-11-24 22:21:00 +1030

    sns_post

---
+++ lib/aws/sns.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+#
+#
+
+sns_post () {
+    if [[ $# -lt 3 ]] ;then
+        "Usage: ${FUNCNAME[0]} STACK_NAME \"SUBJECT\" \"MESSAGE\""
+        return 1
+    fi
+
+    local stack_name="${1-}"
+    local subject="${2-}"
+    local message="${3-}"
+
+    local notification_arn=$(stack_value_output ${stack_name} NotificationTopicArn)
+
+    echoerr "INFO: Publishing to '${notification_arn}'"
+    aws sns publish \
+        --topic-arn ${notification_arn} \
+        --region ${AWS_REGION} \
+        --subject "${subject}" \
+        --message "$(printf ${message} | head -c256k)"
+}
