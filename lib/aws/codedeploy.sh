aae87dc531154669d697aede26d936e50b10cc82
Phil Ingram <phil@communityds.com.au>
2023-04-13 21:36:00 +0930

    improve the efficiency of codedeploy_wrapper if current is present

--- lib/aws/codedeploy.sh
+++ lib/aws/codedeploy.sh
@@ -10,37 +10,45 @@ codedeploy_overlay () {
     local domain_name=${HOSTNAME#*.}
     local stack_name=${domain_name%%.*}
 
-    local deployment_uuid=$(aws deploy get-deployment-group \
-        --region ${AWS_REGION} \
-        --deployment-group-name ${stack_name} \
-        --application-name ${stack_name} \
-        --query "deploymentGroupInfo.deploymentGroupId" \
-        --output text)
+    local deployment_path
 
-    local deployment_base="/opt/codedeploy-agent/deployment-root/${deployment_uuid}"
+    local -a deployment_current=($(find /opt/codedeploy-agent/ -type l -name current 2>/dev/null))
 
-    local deployment_id=$(aws deploy list-deployments \
-        --region ${AWS_REGION} \
-        --application-name ${stack_name} \
-        --deployment-group-name ${stack_name} \
-        --include-only-statuses Succeeded \
-        --max-items 1 \
-        | jq -r '.deployments[0]')
+    if [[ -z ${deployment_current-} ]] || [[ ${#deployment_current[@]} -gt 1 ]]; then
+        local deployment_uuid=$(aws deploy get-deployment-group \
+            --region ${AWS_REGION} \
+            --deployment-group-name ${stack_name} \
+            --application-name ${stack_name} \
+            --query "deploymentGroupInfo.deploymentGroupId" \
+            --output text)
 
-    if [[ ! -d "${deployment_base}/${deployment_id}" ]]; then
-        echoerr "WARNING: Preferred deployment path does not exist: ${deployment_base}/${deployment_id}"
-        local test_deployment_id=$(ls -t1 ${deployment_base} | head -n1)
+        local deployment_base="/opt/codedeploy-agent/deployment-root/${deployment_uuid}"
 
-        if [[ -z ${test_deployment_id-} ]]; then
-            echoerr "ERROR: Codedeploy has not succeeded"
-            return 1
+        local deployment_id=$(aws deploy list-deployments \
+            --region ${AWS_REGION} \
+            --application-name ${stack_name} \
+            --deployment-group-name ${stack_name} \
+            --include-only-statuses Succeeded \
+            --max-items 1 \
+            | jq -r '.deployments[0]')
+
+        if [[ ! -d "${deployment_base}/${deployment_id}" ]]; then
+            echoerr "WARNING: Preferred deployment path does not exist: ${deployment_base}/${deployment_id}"
+            local test_deployment_id=$(ls -t1 ${deployment_base} | head -n1)
+
+            if [[ -z ${test_deployment_id-} ]]; then
+                echoerr "ERROR: Codedeploy has not succeeded"
+                return 1
+            else
+                echoerr "INFO: Found alternate deployment: ${test_deployment_id}"
+                deployment_path="${deployment_base}/${test_deployment_id}/deployment-archive"
+            fi
         else
-            echoerr "INFO: Found alternate deployment: ${test_deployment_id}"
-            local deployment_path="${deployment_base}/${test_deployment_id}/deployment-archive"
+            echoerr "INFO: Found preferred deployment: ${deployment_id}"
+            deployment_path="${deployment_base}/${deployment_id}/deployment-archive"
         fi
     else
-        echoerr "INFO: Found preferred deployment: ${deployment_id}"
-        local deployment_path="${deployment_base}/${deployment_id}/deployment-archive"
+        deployment_path=${deployment_current}
     fi
 
     if [[ ! -d ${deployment_path} ]]; then
